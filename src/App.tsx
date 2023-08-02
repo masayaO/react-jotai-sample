3ab78b8a630f38ecd322ec847b6ff56b409151b6
okkun <okuda4645@gmail.com>
2023-08-02 23:04:23 +0900

    jotai

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { atom, Provider, useAtom } from "jotai";
+import { FC } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const counter = atom(0);
 
+const App: FC = () => {
+  const [count, setCounter] = useAtom(counter);
+  const onClick = () => setCounter((prev) => prev + 1);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Provider>
+      <h1 className="text-3xl font-bold">{count}</h1>
+      <button onClick={onClick} className="text-3xl m-2">
+        Click
+      </button>
+    </Provider>
   );
-}
+};
 
 export default App;
